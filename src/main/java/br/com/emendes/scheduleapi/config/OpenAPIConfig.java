0f9e1c97e05f9c0b0af5cc827172f6537f929e6c
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-06-14 14:11:08 -0300

    Added Springdoc

---
+++ src/main/java/br/com/emendes/scheduleapi/config/OpenAPIConfig.java
@@ -0,0 +1,32 @@
+package br.com.emendes.scheduleapi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+  public static final String SECURITY_SCHEME_KEY = "bearer-key";
+
+  @Bean
+  public OpenAPI openAPI() {
+    Contact contact = new Contact();
+    contact.name("Edson Mendes").email("edson.luiz.mendes@hotmail.com").url("https://github.com/Edson-Mendes");
+
+    SecurityScheme securityScheme = new SecurityScheme()
+        .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
+
+    return new OpenAPI()
+        .info(new Info().title("Schedule API")
+            .description("REST API for schedule management")
+            .version("0.0.1-SNAPSHOT")
+            .contact(contact))
+        .components(new Components().addSecuritySchemes(SECURITY_SCHEME_KEY, securityScheme));
+  }
+
+}
--- src/main/java/br/com/emendes/scheduleapi/config/security/SecurityConfig.java
+++ src/main/java/br/com/emendes/scheduleapi/config/security/SecurityConfig.java
@@ -22,18 +22,21 @@ public class SecurityConfig {
   private final JWTAuthenticationFilter jwtAuthenticationFilter;
 
   private static final String[] POST_WHITELIST = {"/api/auth", "/api/users"};
+  private static final String[] SWAGGER_WHITELIST = {"/swagger-ui.html", "/swagger-ui/**","/webjars/**", "/v3/api-docs/**"};
 
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(
       ServerHttpSecurity http, ReactiveAuthenticationManager authenticationManager) {
     http.csrf().disable()
-        .httpBasic().disable();
+        .httpBasic().disable()
+        .formLogin().disable();
 
     http.authenticationManager(authenticationManager)
         .addFilterBefore(jwtAuthenticationFilter, SecurityWebFiltersOrder.HTTP_BASIC);
 
     http.authorizeExchange()
         .pathMatchers(HttpMethod.POST, POST_WHITELIST).permitAll()
+        .pathMatchers(HttpMethod.GET, SWAGGER_WHITELIST).permitAll()
         .pathMatchers(HttpMethod.GET, "/api/users").hasRole("ADMIN")
         .anyExchange().authenticated();
 
